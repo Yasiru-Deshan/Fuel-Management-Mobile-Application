e2b5c285c2335e3cc37d903ae428fa92362d3ac4
Yasiru-Deshan <yasirudeshan3@gmail.com>
2022-10-21 00:54:56 +0530

    User Interfaces added

--- app/src/main/java/com/example/eadapplication/FuelStation.java
+++ app/src/main/java/com/example/eadapplication/FuelStation.java
@@ -7,18 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Queue;
+
+public class FuelStation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fuel_station);
 
-        Button buttonOne = findViewById(R.id.eligiblebutton);
+        Button buttonOne = findViewById(R.id.join);
         buttonOne.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                Intent activity2Intent = new Intent(getApplicationContext(), FuelStation.class);
+                Intent activity2Intent = new Intent(getApplicationContext(), queue.class);
                 startActivity(activity2Intent);
             }
         });
